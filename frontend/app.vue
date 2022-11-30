6ac3f320506e3cb87591c87af5b8f339422cd6a7
Jacob B. West <westjacob1@gmail.com>
2022-11-30 09:45:27 -0700

    Update app.vue

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,7 +1,7 @@
 <template>
-    <img class="swoop" src="./Mtn-Swoop.png" alt="Swoop" />
+    <img class="swoop" src="@/Mtn-Swoop.png" alt="Swoop" />
   <div class="card">
-    <img class="logo" src="./SDMIA-Logo.png" alt="SDMIA Logo" />
+    <img class="logo" src="@/SDMIA-Logo.png" alt="SDMIA Logo" />
     <!-- <p>Coming Soon</p> -->
   </div>
 </template>
