d3e48a05d1abfa37faaa04d2af113e2a238c672a
Jacob B. West <westjacob1@gmail.com>
2022-11-30 00:05:30 -0700

    Added logo to index

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,41 @@
 <template>
-  <div>
-    <NuxtWelcome />
+    <img class="swoop" src="/mtn-swoop.png" alt="Swoop" />
+  <div class="card">
+    <img class="logo" src="/SDMIA-Logo.png" alt="SDMIA Logo" />
+    <!-- <p>Coming Soon</p> -->
   </div>
 </template>
+
+<style>
+  body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    background-color: #123c62;
+    overflow: hidden;
+    background-repeat: no-repeat;
+  }
+
+  .card {
+    position: absolute;
+    top: 40%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+  }
+
+  .logo {
+    max-width: 1000px;
+    width: 100%;
+    height: auto;
+  }
+
+  .swoop {
+    opacity: 10%;
+    height: auto;
+    position: absolute;
+    bottom: -140px;
+    left: -190px;
+    overflow: hidden;
+  }
+</style>
