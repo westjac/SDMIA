ae60d1e1a050962a896690759022c4f0dc40ba44
Jacob B. West <westjacob1@gmail.com>
2023-05-09 22:20:53 -0600

    Added opre shape for image

--- frontend/components/newsCard.vue
+++ frontend/components/newsCard.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="article-card">
       <div class="article-card__image-wrapper">
-        <img :src="photo" alt="" class="article-card__image" />
+        <img :src="photo" alt="" class="article-card__image OreShapeMask" />
       </div>
       <div class="article-card__content">
         <h3 class="article-card__title">{{ title }}</h3>
@@ -48,15 +48,12 @@
   .article-card__image-wrapper {
     flex-shrink: 0;
     margin-right: 16px;
-    width: 150px;
-    height: 150px;
-    overflow: hidden;
-    border-radius: 50%;
+    
   }
   
   .article-card__image {
-    width: 100%;
-    height: 100%;
+    width: 150px;
+    height: 150px;
     object-fit: cover;
   }
   
@@ -93,6 +90,15 @@
     color: #fff;
   }
 
+  .OreShapeMask {
+    -webkit-mask-image: url("/images/oreShape.png");
+  mask-image: url("/images/oreShape.png");
+  -webkit-mask-repeat: no-repeat;
+  mask-repeat: no-repeat;
+  -webkit-mask-size: 100% 100%;
+  mask-size: 100% 100%;
+  }
+
   /* Media Query for smaller screen sizes */
   @media (max-width: 960px) {
     .article-card {
@@ -112,7 +118,6 @@
     height: 100%;
     object-fit: cover;
     object-position: center;
-    border-radius: 50%;
   }
 
   .article-card__content {
